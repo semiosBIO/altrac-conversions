b289f209de00939e9bfd673a3099625411b5dd4e
Darren <me@darrenholland.com>
2022-02-08 11:42:37 -0600

    lint fixes

--- src/index.js
+++ src/index.js
@@ -992,31 +992,32 @@ const gallonsToAcreFeet = function gallonsToAcreFeet(value, precision) {
   return returnValue;
 };
 
-const litersToUserPreference = function litersToUserPreference(value, userPreference, unitType, precision) {
+function litersToUserPreference(value, userPreference, unitType, precision) {
   let returnValue = 0;
 
   if (!Number.isNaN(value) && Number(value) > 0) {
-    if( unitType === 'volume') {
-    if (userPreference === 'liters') {
-      returnValue = value;
-    } else if (userPreference === 'gallons') {
-      returnValue = round((Number(value) / 3.78541), precision);
-    } else if ( userPreference === 'acreInch') {
-      returnValue = round((Number(value) / 102790.15313), precision);
-    } else if ( userPreference === 'cubicFeet') {
-      returnValue = round((Number(value) / 28.316846592), precision);
-    }else if ( userPreference === 'hectareM') {
-      returnValue = round((Number(value) / 10000000), precision);
-    }else if ( userPreference === 'cubicM') {
-      returnValue = round((Number(value) / 1000), precision);
-    } else {
-      returnValue = round((Number(value) / 1233481.8553199936), precision);
-    }
-  } else if ( unitType === 'flow')
-    if (userPreference === 'lpm') {
-      returnValue = round((Number(value)), precision);
-    } else {
-      returnValue = round((Number(value) / 3.78541), precision);
+    if (unitType === 'volume') {
+      if (userPreference === 'liters') {
+        returnValue = value;
+      } else if (userPreference === 'gallons') {
+        returnValue = round((Number(value) / 3.78541), precision);
+      } else if (userPreference === 'acreInch') {
+        returnValue = round((Number(value) / 102790.15313), precision);
+      } else if (userPreference === 'cubicFeet') {
+        returnValue = round((Number(value) / 28.316846592), precision);
+      } else if (userPreference === 'hectareM') {
+        returnValue = round((Number(value) / 10000000), precision);
+      } else if (userPreference === 'cubicM') {
+        returnValue = round((Number(value) / 1000), precision);
+      } else {
+        returnValue = round((Number(value) / 1233481.8553199936), precision);
+      }
+    } else if (unitType === 'flow') {
+      if (userPreference === 'lpm') {
+        returnValue = round((Number(value)), precision);
+      } else {
+        returnValue = round((Number(value) / 3.78541), precision);
+      }
     }
   }
   return returnValue;
